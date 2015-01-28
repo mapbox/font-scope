787889b63fb85c56dadaae0edff8d7fd9e17922f
Nicki Dlugash <nicki@mapbox.com>
2015-01-28 18:06:46 -0500

    removed backslashes from punctuation character lists, and added name key to language objects in process.js

--- process.js
+++ process.js
@@ -2,33 +2,54 @@ var fs = require('fs'),
     glob = require('glob');
 
 var languages = {};
+var langNamesData = JSON.parse(fs.readFileSync('json/main/en/languages.json'));
+//console.log(langNamesData);
+
 glob.sync('json/main/*/characters.json').map(function(f) {
     var data = JSON.parse(fs.readFileSync(f));
     var languageCode = Object.keys(data.main)[0];
 
-    // ignore the root folder, which is not a language folder
+    // ignore the root folder, which is not a language folder:
     if (languageCode == "root") {
     	return;
-    }
+    };
 
-    var charType = ["exemplarCharacters", "auxiliary", "index"];
+    var allCodePoints = {};
+    // Add the language's full name in English as the first key in a lang's object:
+    allCodePoints["name"] = langNamesData.main.en.localeDisplayNames.languages[languageCode];
+    
+    var charType = ["exemplarCharacters", "auxiliary", "index", "punctuation"];
+
+    // Function to create an array of code points for each charType of a lang:
     var codePointGroup = function(x) {
-    // remove "[" and "]" from character list
-    var charLists = data.main[languageCode].characters[charType[x]].slice(1, -1);
-	var characters = charLists.split(' ');
-	var codePoints = characters.map(function(x) {
-		return x.charCodeAt(0);
-	});	
-	return codePoints;
-	};
+        // remove "[" and "]" from character lists:
+        var charList = data.main[languageCode].characters[charType[x]].slice(1, -1);
+        // remove "//" for now: 
+        if (charType[x] == "punctuation") {
+            charList = charList.replace(/(\\+)/g, function(m, a) {
+                if (a.length === 2) {
+                    return '\\';
+                    } else {
+                        return '';
+                 }
+            });
+        }
 
-	var allCodePoints = {};
-	for (i = 0; i < 3; i++) {
-	allCodePoints[charType[i]]	= codePointGroup(i);
-	};
+	   var characters = charList.split(' ');
+	   var codePoints = characters.map(function(x) {
+	       return x.charCodeAt(0);
+	   });	
+	   return codePoints;
+	}
 
+    // Add keys for all the arrays of code points to a lang's object:
+	for (i = 0; i < 4; i++) {
+	   allCodePoints[charType[i]] = codePointGroup(i);
+	};
+    // Add all the lang objects as keys to the top-level object:
     languages[languageCode] = allCodePoints; 
+
 });
-//console.log(languages);
+
 fs.writeFileSync('output.json', JSON.stringify(languages, null, 2));
 

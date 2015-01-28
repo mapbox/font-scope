4fcdce420a7838e185bf2095f15bcba18149f31f
Nicki Dlugash <nicki@mapbox.com>
2015-01-27 20:03:36 -0500

    added unicode points for auxiliary and index fields. Works but logic needs lots of cleaning up

--- process.js
+++ process.js
@@ -5,15 +5,30 @@ var languages = {};
 glob.sync('json/main/*/characters.json').map(function(f) {
     var data = JSON.parse(fs.readFileSync(f));
     var languageCode = Object.keys(data.main)[0];
+
+    // ignore the root folder, which is not a language folder
+    if (languageCode == "root") {
+    	return;
+    }
+
+    var charType = ["exemplarCharacters", "auxiliary", "index"];
+    var codePointGroup = function(x) {
     // remove "[" and "]" from character list
-    var charList = data.main[languageCode].characters.exemplarCharacters.slice(1, -1);
-	var characters = charList.split(' ');
+    var charLists = data.main[languageCode].characters[charType[x]].slice(1, -1);
+	var characters = charLists.split(' ');
 	var codePoints = characters.map(function(x) {
 		return x.charCodeAt(0);
-	});
+	});	
+	return codePoints;
+	};
 
-    languages[languageCode] = codePoints;
+	var allCodePoints = {};
+	for (i = 0; i < 3; i++) {
+	allCodePoints[charType[i]]	= codePointGroup(i);
+	};
+
+    languages[languageCode] = allCodePoints; 
 });
-console.log(languages);
+//console.log(languages);
 fs.writeFileSync('output.json', JSON.stringify(languages, null, 2));
 
